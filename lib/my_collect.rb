6d9511a678591f50605ef8f17baf44f8cc7ccb4b
Albina Zakirova <zak1804@gmail.com>
2018-11-10 18:12:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,9 +6,8 @@ def my_collect(collection)
   i = 0
   
     while i < collection.length
-      new_collection <<  collection[i].split.first
-       yield
-       i += 1
+      new_collection <<  yield collection.split.first
+      i += 1
     end
    new_collection
 end
