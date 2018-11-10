b0477f395a101fe5b01563963a013782a169aab7
Albina Zakirova <zak1804@gmail.com>
2018-11-10 18:00:00 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,14 @@
-def my_collect
-  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-  string = array.join(' ')
-  array = string.split(',')
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  
+def my_collect(collection)
+  new_collection=[]
+ # array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
   i = 0
-  while i < array.length
-   new_array << yield array[i]
-   i ++ =1
+  while i < collection.length
+     new_collection=collection[i].splite(' ')
+     new_collection << yield new_collection.first
+     i += 1
   end
-  new_array
+  new_collection
 end
 
