35e8256421c9e93cef9b18c5f480c863c70ea88a
Albina Zakirova <zak1804@gmail.com>
2018-11-10 17:10:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect
+  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  string = array.join
+  array = string.split(',')
+  i = 0
+  while i < array.length
+   new_array << array[]
+   i++=1
+  end
+  puts new_array
+end
 
