854f0da1864bbbb9909ff6eb349ce6bd15c16292
Albina Zakirova <zak1804@gmail.com>
2018-11-10 18:06:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,11 @@ collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
   
 def my_collect(collection)
   new_collection=[]
- # array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
   i = 0
   while i < collection.length
-     new_collection << yield collection[i].split.first
-     
+     new_collection <<  collection[i].split.first
+     yield
      i += 1
   end
   new_collection
